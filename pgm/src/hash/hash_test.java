51d5738493fc9378c25e5d9f7b20d790563abe90
parkjieun <wong_303@naver.com>
2022-07-01 18:02:46 +0900

    sort

---
+++ pgm/src/hash/hash_test.java
@@ -0,0 +1,12 @@
+package hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class hash_test {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap();
+        //map.getOrDefault('key','기본값'); //- 찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환하는 메서드
+
+    }
+}
